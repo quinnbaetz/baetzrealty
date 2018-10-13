0875e34146b6389f81c91e076ab2745e659b525c
Quinn Baetz <quinnbaetz@gmail.com>
2018-10-13 16:51:10 -0700

    Using spinner for iframe, not good enough

--- src/pages/SearchProperty.js
+++ src/pages/SearchProperty.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import 'pages/SearchProperty.scss'
-const Test = () => (
-   <div className='SearchProperty'>
-     <iframe
-       width='100%'
-       height="100%"
-       scrolling="auto"
-       src="https://nataliabaetz.realscout.com/homesearch/map?ptype=SFR,TC&clat=37.81670030664058&clng=-122.22736358642577&zoom=12&pmin=2000000"
-     />
-   </div>
- );
+import Spinner from 'react-spinkit'
 
-export default Test
+var url = "https://nataliabaetz.realscout.com/homesearch/map?ptype=SFR,TC&clat=37.81670030664058&clng=-122.22736358642577&zoom=12&pmin=2000000"
+
+class SearchProperty extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {loading: true}
+  }
+  onLoad = () => {
+    this.setState({
+      loading: false
+    })
+  }
+  render(){
+      const {loading} = this.state
+      return (
+        <div className='SearchProperty'>
+          <iframe
+            style={{
+              display: loading ? 'none' : 'block'
+            }}
+            width='100%'
+            height="100%"
+            scrolling="auto"
+            src={url}
+            onLoad={this.onLoad}
+          />
+
+          {loading && <div style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%"
+          }}>
+            <Spinner name="ball-spin-fade-loader" />
+          </div>}
+        </div>
+      );
+  }
+}
+
+export default SearchProperty
