c4286744de813c6c42f1d405d4e48b68f9db14b9
Quinn Baetz <quinnbaetz@gmail.com>
2018-10-13 15:57:21 -0700

    using drift to talk to customers

--- src/index.js
+++ src/index.js
@@ -6,24 +6,13 @@ import * as serviceWorker from './serviceWorker';
 import './i18n';
 import 'index.scss'
 import { BrowserRouter } from 'react-router-dom';
-import {Helmet} from 'react-helmet'
 
 
 ReactDOM.render((
-  <fragment>
-    <Helmet>
-      <Helmet
-        script={[{
-          type: 'text/javascript',
-          innerHTML: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
-          analytics.load("1dQDk9cbEjyQC2yxDWOtfYfUuqBSgAHl");
-          analytics.page();
-        }}();`}]} />
-    </Helmet>
-    <BrowserRouter>
-      <Page />
-    </BrowserRouter>
-  </fragment>), document.getElementById('root'));
+  <BrowserRouter>
+    <Page />
+  </BrowserRouter>
+  ), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
